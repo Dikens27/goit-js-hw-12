1a953edce6290320e17b01f79f91a11c1bae4f9c
Dikens27 <141275668+Dikens27@users.noreply.github.com>
2024-01-18 21:36:48 +0000

    Deploying to gh-pages from @ Dikens27/goit-js-hw-12@b67578faca953007b75c039ab1a557de820822ba 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,29 +1,29 @@
-import{a as y,S as g,i as u}from"./assets/vendor-bad0427b.js";(function(){const a=document.createElement("link").relList;if(a&&a.supports&&a.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const l of r.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&s(l)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();const h=document.querySelector(".form"),c=document.querySelector(".gallery"),d=document.querySelector(".loader"),m=document.querySelector(".load-more");let f=1;const p=40;h.addEventListener("submit",async t=>{t.preventDefault(),S();const a=t.currentTarget.elements.search.value.trim();if(a.length===0)i("Sorry, there are no images matching your search query. Please try again!Reqest is not ok"),n();else try{const o=await L(a);w(o),m.style.display="block"}catch(o){console.error(o),i("Sorry, there are no images matching your search query. Please try again!Reqest is not ok")}finally{n()}});async function L(t=""){const a=y.create({baseURL:"https://pixabay.com/api/",headers:{"Content-Type":"application/json"},params:{key:"41729431-93e496ed3cd794296b45db789",q:t,image_type:"photo",orientation:"horizontal",safesearch:"true",page:f,per_page:p}});try{return(await a.get()).data}catch{i("Sorry, there are no images matching your search query. Please try again!Reqest is not ok")}const o=await response.json();return o.hits.length?o.hits:(i("Sorry, there are no images matching your search query. Please try again!Reqest is not ok"),n(),[])}const q=({largeImageURL:t,webformatURL:a,tags:o,likes:s,views:e,comments:r,downloads:l})=>`           
+import{a as b,S as w,i as k}from"./assets/vendor-bad0427b.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))o(r);new MutationObserver(r=>{for(const a of r)if(a.type==="childList")for(const i of a.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function l(r){const a={};return r.integrity&&(a.integrity=r.integrity),r.referrerpolicy&&(a.referrerPolicy=r.referrerpolicy),r.crossorigin==="use-credentials"?a.credentials="include":r.crossorigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function o(r){if(r.ep)return;r.ep=!0;const a=l(r);fetch(r.href,a)}})();const S=document.querySelector(".form"),m=document.querySelector(".gallery"),f=document.querySelector(".loader"),n=document.querySelector(".load-more");let d=1,h=!1;const u=40;let y,g;S.addEventListener("submit",async e=>{if(e.preventDefault(),d=1,L(),m.innerHTML="",y=e.currentTarget.elements.search.value.trim(),y.length===0)s("Sorry, there are no images matching your search query. Please try again!Reqest is not ok"),c();else try{n.style.display="none";const t=await p(y);q(t)}catch(t){console.error(t),s("Sorry, there are no images matching your search query. Please try again!Reqest is not ok")}finally{c()}});async function p(e=""){const t=b.create({baseURL:"https://pixabay.com/api/",headers:{"Content-Type":"application/json"},params:{key:"41729431-93e496ed3cd794296b45db789",q:e,image_type:"photo",orientation:"horizontal",safesearch:"true",page:d,per_page:u}});try{const o=await(await t.get()).data;if(console.log(o),o.hits.length)return g=o.totalHits,o.hits;s("Sorry, there are no images matching your search query. Please try again!Reqest is not ok"),c();return}catch{s("Sorry, there are no images matching your search query. Please try again!Reqest is not ok");return}}const v=async()=>{try{if(h){s("We're sorry, but you've reached the end of search results.");return}d++;try{L(),n.style.display="none";const e=await p(y);q(e)}catch(e){console.error(e),s("Sorry, there are no images matching your search query. Please try again!Reqest is not ok")}finally{c()}d>=g/u&&(h=!0,n.style.display="none",s("We're sorry, but you've reached the end of search results."))}catch{s("Sorry, there are no images matching your search query. Please try again!Reqest is not ok")}};n.addEventListener("click",e=>{v()});const P=({largeImageURL:e,webformatURL:t,tags:l,likes:o,views:r,comments:a,downloads:i})=>`           
     <li class="gallery-item">
         <div class=gallery-card>
-            <a class="gallery-link" href="${t}">
-                <img class="gallery-image" src="${a}" alt="${o}" width:"360" height:"200" />
+            <a class="gallery-link" href="${e}">
+                <img class="gallery-image" src="${t}" alt="${l}" width:"360" height:"200" />
             </a>
 
             <ul class="gallery-card-list">
                 <li class="gallery-card-item">
                     <h2 class="gallery-card-title">Likes</h2>
-                    <p class="gallery-card-information">${s}</p>
+                    <p class="gallery-card-information">${o}</p>
                 </li>
                 <li class="gallery-card-item">
                     <h2 class="gallery-card-title">Views</h2>
-                    <p class="gallery-card-information">${e}</p>
-                </li>
-                <li class="gallery-card-item">
-                    <h2 class="gallery-card-title">Comments</h2>
                     <p class="gallery-card-information">${r}</p>
                 </li>
                 <li class="gallery-card-item">
+                    <h2 class="gallery-card-title">Comments</h2>
+                    <p class="gallery-card-information">${a}</p>
+                </li>
+                <li class="gallery-card-item">
                     <h2 class="gallery-card-title">Downloads</h2>
-                    <p class="gallery-card-information">${l}</p>
+                    <p class="gallery-card-information">${i}</p>
                 </li>
             </ul>
         </div>
     </li>
-`,b=new g(".gallery a",{captionsData:"alt",captionDelay:250});function w(t){if(c.innerHTML="",t!==void 0){const a=t.hits.map(o=>q(o)).join("");c.insertAdjacentHTML("beforeend",a),b.refresh(),n()}}function i(t){u.show({title:t,titleColor:"white",color:"white",backgroundColor:"red",position:"topRight"})}function S(){d.classList.add("loading")}function n(){d.classList.remove("loading")}
+`,M=new w(".gallery a",{captionsData:"alt",captionDelay:250});function q(e){if(console.log(e),e!==void 0){const t=e.map(l=>P(l)).join("");m.insertAdjacentHTML("beforeend",t),M.refresh(),c(),Math.floor(g/u)>0&&(n.style.display="block")}}function s(e){k.show({title:e,titleColor:"white",color:"white",backgroundColor:"red",position:"topRight"})}function L(){f.classList.add("loading")}function c(){f.classList.remove("loading")}
 //# sourceMappingURL=commonHelpers.js.map
